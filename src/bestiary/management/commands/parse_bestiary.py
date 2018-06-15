7275bdab447063a959fcd00d07bee0b980ac5a95
Peter <peter@porksmash.com>
2018-06-14 18:31:28 -0700

    Added management command to parse bestiary files

---
+++ src/bestiary/management/commands/parse_bestiary.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+from bestiary import parse
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        parse.creatures()
