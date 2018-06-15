5efbc47acc744fc9a25ec9f846f4ad1fde800005
Peter <peter@porksmash.com>
2018-06-14 18:55:36 -0700

    oops

--- src/mmegfarm/settings.py
+++ src/mmegfarm/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': os.environ.get('RDS_DB_NAME'),
         'USER': os.environ.get('RDS_USERNAME'),
         'PASSWORD': os.environ.get('RDS_PASSWORD'),
-        'HOST': os.environ.get(['RDS_HOSTNAME']),
+        'HOST': os.environ.get('RDS_HOSTNAME'),
         'PORT': os.environ.get('RDS_PORT'),
     }
 }
