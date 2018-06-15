0ca5fd13cd3cc14238a9d50f394e65cb3148145f
Peter Andersen <peter@porksmash.com>
2018-06-15 12:57:09 -0700

    Fixed awsebcli package name. Removed all login/out cruft and moved API to root URL.

--- src/mmegfarm/urls.py
+++ src/mmegfarm/urls.py
@@ -1,13 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls', namespace='api')),
-    path('', TemplateView.as_view(template_name='base.html'))
+    path('', include('api.urls', namespace='api')),
 ]
 
 if settings.DEBUG:
