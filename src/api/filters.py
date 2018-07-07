51f78dbbaba2dcc6fbc911cca045d37dcee84e14
Peter Andersen <peter@porksmash.com>
2018-07-07 12:20:35 -0700

    Added scales_with filter

--- src/api/filters.py
+++ src/api/filters.py
@@ -34,10 +34,7 @@ class CreatureFilter(filters.FilterSet):
     spell_effect = filters.CharFilter(name='spell__spelleffect__effect', method=filter_has_all_related)
     spell_effect_any = filters.CharFilter(name='spell__spelleffect__effect__in', method=filter_array_value)
     spell_target = filters.CharFilter(name='spell__spelleffect__target__in', method=filter_array_value)
-    spell_params_contains = filters.CharFilter(
-        name='spell__spelleffect__params__has_keys',
-        method=filter_array_value
-    )
+    scales_with = filters.CharFilter(name="spell__spelleffect__params__incBase__in", method=filter_array_value)
 
     class Meta:
         model = Creature
